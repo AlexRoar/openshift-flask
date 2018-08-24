80ac4d67128de56315325c89631802a192a87499
Denis Zuev <root@dzuev.pro>
2018-08-24 02:34:41 -0400

    updated flask files

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ def f_health():
 
 @app.route('/')
 def f_root():
-    return '<h1>Hello, World!</h1> <p> This container hostname is: {}</p>'.format(os.environ['HOSTNAME'])
+    # return '<h1>Hello, World!</h1> <p> This container hostname is: {}</p>'.format(os.environ['HOSTNAME'])
+    return '<h1>Hello, World!</h1> <p> We are in <b>{}</b></p> <p> This container hostname is: {}</p>'.format(os.environ['STAGE'],os.environ['HOSTNAME'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080,debug=True)
